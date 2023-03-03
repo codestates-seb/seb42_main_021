be53b8ab1514c3a7c486f9f1955867b766736115
KBM <kimbm1114@gmail.com>
2023-03-04 01:07:06 +0900

    백엔드 폴더 추가

---
+++ Team021/src/main/java/seb42/main1/Team021Application.java
@@ -0,0 +1,13 @@
+package seb42.main1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team021Application.class, args);
+	}
+
+}
---
+++ Team021/src/test/java/seb42/main1/Team021ApplicationTests.java
@@ -0,0 +1,13 @@
+package seb42.main1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
