7d193585145fe927515c0bceea9a73ac92b4bc92
LegendPaino <87295495+LegendPaino@users.noreply.github.com>
2023-03-07 11:32:36 +0900

    chore: Spring Application initializr
    1. Spring Application 설치
    2. Spring 기본 의존(boot, security, lombok, h2) 설치
    DTO 유효성 검증 라이브러리 validation 설치
    3. Json 파싱 및 생성 라이브러리 gson 설치
    4. Rest API Docs 라이브러리 설치 및 세팅
    5. 매핑 클래스 자동 생성기 라이브러리 mapstruct 설치
    6. Spring Rest Docs html 파일 Git에 Add 못하게 gitignore 추가

---
+++ server/EverybodyChachapark/src/main/java/com/DuTongChitongYutong/EverybodyChachapark/EverybodyChachaparkApplication.java
@@ -0,0 +1,13 @@
+package com.DuTongChitongYutong.EverybodyChachapark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverybodyChachaparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverybodyChachaparkApplication.class, args);
+	}
+
+}
