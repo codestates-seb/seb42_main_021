62c647a0efde774f1d231e422aff500952c7beb2
MingGyuKang <beauty8536@naver.com>
2023-03-08 22:31:42 +0900

    feat: zustand 구현

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,19 @@
+// import useStore from '../components/useStore';
 import Main from '../components/main/Main';
+
+// const Home = () => {
+//   const { bears, increasePopulation, removeAllBears } = useStore();
+//   return (
+//     <Main>
+//       <button onClick={increasePopulation}>{bears}</button>
+//       <button onClick={removeAllBears}>초기화</button>
+//     </Main>
+//   );
+// };
+
+// export default Home;
+
+import React from 'react';
 
 const Home = () => {
   return <Main></Main>;
