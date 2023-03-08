ea014c52677c28f9bb48a7020f87292e05873e3a
MingGyuKang <beauty8536@naver.com>
2023-03-08 20:18:02 +0900

    docs: 컴포넌트 정리

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 const Container = styled.div`
   height: 100vh;
   width: 100%;
-  background-color: black;
+  max-width: 530px;
+  background-color: gray;
+  opacity: 0.5;
+  position: fixed;
+  top: 80px;
 `;
 
 const Main = () => {
--- client/src/components/main/MainHeader.js
+++ client/src/components/main/MainHeader.js
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  z-index: 1000;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   width: 100%;
   max-width: 530px;
   height: 80px;
-  background-color: white;
+  background-color: green;
   position: fixed;
   top: 0;
 `;
