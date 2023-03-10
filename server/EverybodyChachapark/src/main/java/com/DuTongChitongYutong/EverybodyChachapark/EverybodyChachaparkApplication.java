f30576699f6e8e358e23c61e717aacd8da7128fa
KBM <kimbm1114@gmail.com>
2023-03-11 00:53:08 +0900

    feat application 수정

--- server/EverybodyChachapark/src/main/java/com/DuTongChitongYutong/EverybodyChachapark/EverybodyChachaparkApplication.java
+++ server/EverybodyChachapark/src/main/java/com/DuTongChitongYutong/EverybodyChachapark/EverybodyChachaparkApplication.java
@@ -3,12 +3,7 @@ package com.DuTongChitongYutong.EverybodyChachapark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.CrossOrigin;
-=======
->>>>>>> db83303be000cb95a3427d1c1da6fb893007d3d7
 
-@CrossOrigin
 @EnableJpaAuditing
 @SpringBootApplication
 public class EverybodyChachaparkApplication {
