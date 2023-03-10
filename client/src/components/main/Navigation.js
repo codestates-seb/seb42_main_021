17e0dc70a4da75c1248152e2578a88a377929c8e
EunjiLee1997 <eunjilee1111@gmail>
2023-03-10 17:27:04 +0900

    [design] footer, navigation 디자인 수정

--- client/src/components/main/Navigation.js
+++ client/src/components/main/Navigation.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
+import { Link, useLocation } from 'react-router-dom';
+import {
+  FaHome,
+  FaSearch,
+  FaShoppingBag,
+  FaShoppingCart,
+  FaUser,
+} from 'react-icons/fa';
 
-const Container = styled.nav`
+const NavigationLayout = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   z-index: 1000;
   width: 100%;
   max-width: 530px;
@@ -10,10 +21,67 @@ const Container = styled.nav`
   bottom: 0;
   border: none;
   border-radius: var(--bd-rd);
+  padding: 16px;
+`;
+
+const NavigationMenu = styled.ul`
+  display: flex;
+  width: 100%;
+  > li {
+    margin: 0 auto;
+  }
 `;
 
 const Navigation = () => {
-  return <Container></Container>;
+  const location = useLocation();
+
+  return (
+    <NavigationLayout>
+      <NavigationMenu>
+        <li>
+          <Link to="/">
+            {location.pathname === '/' ? (
+              <FaHome size="50px" color="#61a0ff" />
+            ) : (
+              <FaHome size="50px" color="#4E4E4E" />
+            )}
+          </Link>
+        </li>
+        <li>
+          <Link to="/product">
+            <FaSearch size="50px" color="#4E4E4E" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/product">
+            {location.pathname === '/product' ? (
+              <FaShoppingBag size="50px" color="#61a0ff" />
+            ) : (
+              <FaShoppingBag size="50px" color="#4E4E4E" />
+            )}
+          </Link>
+        </li>
+        <li>
+          <Link to="/shoppingcart/:id">
+            {location.pathname === '/shoppingcart/:id' ? (
+              <FaShoppingCart size="50px" color="#61a0ff" />
+            ) : (
+              <FaShoppingCart size="50px" color="#4E4E4E" />
+            )}
+          </Link>
+        </li>
+        <li>
+          <Link to="/mypage/:id">
+            {location.pathname === '/mypage/:id' ? (
+              <FaUser size="50px" color="#61a0ff" />
+            ) : (
+              <FaUser size="50px" color="#4E4E4E" />
+            )}
+          </Link>
+        </li>
+      </NavigationMenu>
+    </NavigationLayout>
+  );
 };
 
 export default Navigation;
