b72be0fd4c8ab0430493798ddc73c2bfe0f56975
MingGyuKang <beauty8536@naver.com>
2023-03-18 16:10:57 +0900

    [feat] ngrok 코드 제거

--- client/src/components/api/itemAPI.js
+++ client/src/components/api/itemAPI.js
@@ -6,7 +6,6 @@ export const getProductList = async (page, size) => {
       method: 'get',
       url: `/products/all?page=${page}&size=${size}`,
       headers: {
-        'ngrok-skip-browser-warning': '12',
         'Content-Type': 'application/json',
       },
     });
@@ -23,11 +22,7 @@ export const categoryProductList = async (categoryFilter) => {
   try {
     const response = await axios({
       method: 'get',
-      url: `/products/category/${categoryFilter}?&page=1&size=10`,
-      headers: {
-        'ngrok-skip-browser-warning': '12',
-        'Content-Type': 'application/json',
-      },
+      url: `/products/category/${categoryFilter}?page=1&size=10`,
     });
     console.log('categoryProductList 받아오기 성공');
     console.log(response.data);
@@ -39,17 +34,17 @@ export const categoryProductList = async (categoryFilter) => {
 };
 
 export const searchProductList = async (keyword) => {
+  console.log(keyword);
   try {
     const response = await axios({
       method: 'get',
-      url: `/products/search?keyword=${keyword}&page=1&size=5`,
+      url: `http://localhost:8080/products/search?searchKeyword=${keyword}&page=1&size=10`,
       headers: {
-        'ngrok-skip-browser-warning': '12',
         'Content-Type': 'application/json',
       },
     });
     console.log('searchProductList 받아오기 성공');
-    console.log(response.data);
+    console.log(response);
     return response.data;
   } catch (error) {
     console.log('searchProductList 받아오기 실패');
