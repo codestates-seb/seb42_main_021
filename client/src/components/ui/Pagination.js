8724f4b2a74bdf25b3e4fc40121185d5329b7be9
MingGyuKang <beauty8536@naver.com>
2023-03-20 00:22:24 +0900

    [feat] 상품 목록 페이지네이션 기능 구현

---
+++ client/src/components/ui/Pagination.js
@@ -0,0 +1,23 @@
+import '../../Paging.css';
+import Pagination from 'react-js-pagination';
+
+const Paging = ({ page, count, setPage }) => {
+  const handlePageChange = (page) => {
+    setPage(page - 1);
+  };
+  console.log();
+
+  return (
+    <Pagination
+      activePage={page - 1}
+      itemsCountPerPage={10}
+      totalItemsCount={count}
+      pageRangeDisplayed={5}
+      prevPageText={'‹'}
+      nextPageText={'›'}
+      onChange={handlePageChange}
+    />
+  );
+};
+
+export default Paging;
