1ba67999451c2bd4073392d76555d431baa46d14
MingGyuKang <beauty8536@naver.com>
2023-03-20 00:21:53 +0900

    [feat][refactor] 전체 상품 조회 리팩토링, 카테고리 및 검색 상품 조회 기능 구현

--- client/src/components/api/itemAPI.js
+++ client/src/components/api/itemAPI.js
@@ -9,9 +9,9 @@ export const getProductList = async (page, size) => {
         'Content-Type': 'application/json',
       },
     });
-    console.log(response.data);
+    console.log(response);
     console.log('getItemList 받아오기 성공');
-    return response.data;
+    return response;
   } catch (error) {
     console.log('getItemList 받아오기 실패');
     console.log(error);
@@ -22,11 +22,11 @@ export const categoryProductList = async (categoryFilter) => {
   try {
     const response = await axios({
       method: 'get',
-      url: `/products/category/${categoryFilter}?page=1&size=10`,
+      url: `/products/category?category=${categoryFilter}&page=0&size=10`,
     });
     console.log('categoryProductList 받아오기 성공');
-    console.log(response.data);
-    return response.data;
+    console.log(response);
+    return response;
   } catch (error) {
     console.log('categoryProductList 받아오기 실패');
     console.log(error);
@@ -38,14 +38,14 @@ export const searchProductList = async (keyword) => {
   try {
     const response = await axios({
       method: 'get',
-      url: `http://localhost:8080/products/search?searchKeyword=${keyword}&page=1&size=10`,
+      url: `/products/search?searchKeyword=${keyword}&page=0&size=10`,
       headers: {
         'Content-Type': 'application/json',
       },
     });
     console.log('searchProductList 받아오기 성공');
     console.log(response);
-    return response.data;
+    return response;
   } catch (error) {
     console.log('searchProductList 받아오기 실패');
     console.log(error);
