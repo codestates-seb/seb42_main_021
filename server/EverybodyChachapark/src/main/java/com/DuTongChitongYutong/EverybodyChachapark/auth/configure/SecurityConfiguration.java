c4803b8beab91ee9fc2279a22f1784d305163992
KBM <kimbm1114@gmail.com>
2023-03-23 13:20:53 +0900

    [Feat] oAuth2 적용 최종본

--- server/EverybodyChachapark/src/main/java/com/DuTongChitongYutong/EverybodyChachapark/auth/configure/SecurityConfiguration.java
+++ server/EverybodyChachapark/src/main/java/com/DuTongChitongYutong/EverybodyChachapark/auth/configure/SecurityConfiguration.java
@@ -80,7 +80,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers(HttpMethod.POST, "/members/signup").permitAll()
                         .antMatchers(HttpMethod.PATCH, "/members").hasRole("USER")
-                        .antMatchers(HttpMethod.GET, "/members/mypage").hasRole("USER")
+                        .antMatchers(HttpMethod.GET, "/members").hasRole("USER")
                         .antMatchers(HttpMethod.DELETE, "/members").hasRole("USER")
                         .antMatchers(HttpMethod.POST, "/carts").hasRole("USER")
                         .antMatchers(HttpMethod.GET, "/carts").hasRole("USER")
@@ -95,7 +95,7 @@ public class SecurityConfiguration {
                         .anyRequest().permitAll()
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .successHandler(new Oauth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberService))
+                        .successHandler(new Oauth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberService, refreshTokenRepository))
                 );
         return http.build();
     }
